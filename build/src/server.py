4f6361d8cbe00d82bc51f6bcb1af70a36778aef1
Jacques Kvam <jacques@verdigris.co>
2016-08-03 00:22:40 -0700

    use $PORT

--- build/src/server.py
+++ build/src/server.py
@@ -52,4 +52,6 @@ def _(*args):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = os.environ.get('PORT', 5000)
+    print(port)
+    socketio.run(app, host='0.0.0.0', port=port)
