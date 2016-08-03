135d57ccdaad2c2d48a8e0fa8134dcc9eff1d9ca
Jacques Kvam <jacques@verdigris.co>
2016-08-03 00:03:20 -0700

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python build/src/server.py
---
+++ build/src/server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from flask import Flask, render_template, copy_current_request_context
+from flask_socketio import SocketIO, emit
+import eventlet
+
+sys.path.insert(0, '/Users/jacques/dev/bowtie-demo')
+
+import example
+
+app = Flask(__name__)
+app.debug = False
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('5#click')
+def _(*args):
+
+    foo = copy_current_request_context(example.foo2)
+    eventlet.spawn(foo, *args)
+
+
+@socketio.on('1#change')
+def _(*args):
+
+    foo = copy_current_request_context(example.foo)
+    eventlet.spawn(foo, *args)
+
+
+@socketio.on('4#change')
+def _(*args):
+
+    foo = copy_current_request_context(example.foo3)
+    eventlet.spawn(foo, *args)
+
+
+@socketio.on('3#click')
+def _(*args):
+
+    foo = copy_current_request_context(example.newtable)
+    eventlet.spawn(foo, *args)
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
